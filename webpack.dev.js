3939abeba5d8235075bbdc6188ebd933210cf5e2
Teoxoy <28601907+Teoxoy@users.noreply.github.com>
2018-09-14 20:51:34 +0200

    switched from webpack to parcel

--- webpack.dev.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const merge = require('webpack-merge')
-const common = require('./webpack.common.js')
-
-module.exports = merge(common, {
-    mode: 'development'
-    // devtool: 'inline-source-map'
-})
