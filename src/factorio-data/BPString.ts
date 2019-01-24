9994807f76dbaca9f3437ff0f1f46d6aa1f502ca
Teoxoy <28601907+Teoxoy@users.noreply.github.com>
2019-01-24 14:18:15 +0100

    updated allorigins url (https://github.com/gnuns/AllOrigins/issues/33)

--- src/factorio-data/BPString.ts
+++ src/factorio-data/BPString.ts
@@ -87,7 +87,7 @@ function findBPString(data: string) {
     // Other CORS Proxies:
     // https://crossorigin.me/
     // https://cors-anywhere.herokuapp.com/
-    const corsProxy = 'https://api.allorigins.ml/get?method=raw&url='
+    const corsProxy = 'https://api.allorigins.ml/raw?url='
 
     // TODO: maybe add dropbox support https://www.dropbox.com/s/ID?raw=1
     return new Promise(resolve => resolve(new URL(DATA))).then((url: URL) => {
