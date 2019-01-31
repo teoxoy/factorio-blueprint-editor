a8524b59e7bc2fc13809c95a581c1eeefb3ce81d
SIPS1980 <SIPS1980@outlook.com>
2019-01-31 17:41:05 -0500

    * Finalized Beacon Editor

--- src/editors/beacon.ts
+++ src/editors/beacon.ts
@@ -1,4 +1,6 @@
 import Editor from './editor'
+import Preview from './components/preview'
+import Modules from './components/modules'
 import Entity from '../factorio-data/entity'
 
 /** Beacon Editor */
@@ -6,5 +8,13 @@ export default class BeaconEditor extends Editor {
 
     constructor(entity: Entity) {
         super(402, 171, entity)
+
+        // Add Preview
+        const preview: Preview = this.addPreview()
+
+        // Add Modules
+        this.addLabel(140, 56, 'Modules:')
+        const modules: Modules = this.addModules(208, 45)
+        modules.on('changed', () => preview.redraw())
     }
 }
