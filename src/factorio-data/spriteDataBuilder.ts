teoxoy/factorio-blueprint-editor

commit d77240e9edf3d8053d27c9ba0ed63386a02e239b

removed ability to move entities with the mouse added ability to move entities with the arrow keys removed immutable.js from positionGrid

src/factorio-data/spriteDataBuilder.ts
@@ -231,7 +231,7 @@ function getPipeConnectionPoints(e: any, dir: number, assemblerPipeDirection: st
     return positions
 }
 
-function getHeatConectionPoints(e: any) {
+function getHeatConectionPoints(e: FD.Entity) {
     // nuclear reactor
     if (e.heat_buffer) return e.heat_buffer.connections
     // heat exchanger
@@ -239,29 +239,37 @@ function getHeatConectionPoints(e: any) {
 }
 
 function getHeatConnections(position: IPoint, bp: Blueprint) {
-    return bp.entityPositionGrid.getSurroundingEntities(new Area(position), (entnr, _, X, Y) => {
-        const entity = bp.entities.get(entnr)
-        // tslint:disable-next-line:prefer-switch
-        if (entity.name === 'heat_pipe') return true
-        else if (entity.name === 'heat_exchanger' || entity.name === 'nuclear_reactor') {
-            const connections = getHeatConectionPoints(entity.entityData)
-            for (const connection of connections) {
-                const offset = util.rotatePointBasedOnDir(connection.position, entity.direction)
-                if (X === Math.floor(entity.position.x + offset.x) &&
-                    Y === Math.floor(entity.position.y + offset.y)) return true
+    return bp.entityPositionGrid.getNeighbourData(position)
+        .map(({ x, y, entity }) => {
+            if (!entity) return false
+
+            if (entity.name === 'heat_pipe') return true
+            if (entity.name === 'heat_exchanger' || entity.name === 'nuclear_reactor') {
+                getHeatConectionPoints(entity.entityData)
+                    .map(conn => util.rotatePointBasedOnDir(conn.position, entity.direction))
+                    .forEach(offset => {
+                        if (x === Math.floor(entity.position.x + offset.x) &&
+                            y === Math.floor(entity.position.y + offset.y)) return true
+                    })
             }
-        }
-    })
+
+            return false
+        })
 }
 
 function getBeltConnections2(bp: Blueprint, position: IPoint, dir: number) {
-    const directions = bp.entityPositionGrid.getSurroundingEntities(new Area(position), entnr => {
-        const entity = bp.entities.get(entnr)
-        if (entity.type === 'transport_belt' ||
-        entity.type === 'splitter' ||
-        (entity.type === 'underground_belt' &&
-        entity.directionType === 'output')) return entity.direction
-    }, dir)
+    let directions = bp.entityPositionGrid.getNeighbourData(position)
+        .map(({ entity }) => {
+            if (!entity) return false
+
+            if (
+                entity.type === 'transport_belt' ||
+                entity.type === 'splitter' ||
+                (entity.type === 'underground_belt' && entity.directionType === 'output')
+            ) return entity.direction
+        })
+    // Rotate directions
+    directions = [...directions, ...directions].splice(dir / 2, 4)
 
     const foundR = directions[1] !== false && (directions[1] + 2) % 8 === dir
     const foundL = directions[3] === (dir + 2) % 8
@@ -510,7 +518,7 @@ function generateGraphics(e: any) {
                     y: data.position.y,
                     width: size.x,
                     height: size.y
-                }, true), (entnr: number) => {
+                }), (entnr: number) => {
                     const ent = data.bp.entities.get(entnr)
                     if (ent && ent.name === 'gate') return true
                 }, true)
@@ -597,10 +605,9 @@ function generateGraphics(e: any) {
                     return e.pictures[type].layers[0]
                 }
                 if (data.bp) {
-                    const conn = data.bp.entityPositionGrid.getSurroundingEntities(new Area(data.position), (entnr: number, D: number) => {
-                        const entity = data.bp.entities.get(entnr)
-                        if (entity.name === 'stone_wall' || (entity.name === 'gate' && entity.direction === D % 4)) return true
-                    })
+                    const conn = data.bp.entityPositionGrid.getNeighbourData(data.position)
+                        .map(({ entity, relDir }) =>
+                            entity && (entity.name === 'stone_wall' || (entity.name === 'gate' && entity.direction === relDir % 4)))
 
                     if (conn[1] && conn[2] && conn[3]) return getRandomPic('t_up')
                     if (conn[1] && conn[2]) return getRandomPic('corner_right_down')
@@ -614,11 +621,10 @@ function generateGraphics(e: any) {
             }
 
             if (data.bp) {
-                let found = false
-                data.bp.entityPositionGrid.getSurroundingEntities(new Area(data.position), (entnr: any, D: number) => {
-                    const entity = data.bp.entities.get(entnr)
-                    if (entity.name === 'gate' && entity.direction === D % 4) found = true
-                })
+                const found = data.bp.entityPositionGrid.getNeighbourData(data.position)
+                    .find(({ entity, relDir }) =>
+                        entity && entity.name === 'gate' && entity.direction === relDir % 4)
+
                 if (found && !data.hasConnections) return [getBaseSprite(), e.wall_diode_red]
             }
             return [getBaseSprite()]
@@ -633,7 +639,7 @@ function generateGraphics(e: any) {
                         y: data.position.y,
                         width: size.x,
                         height: size.y
-                    }, true), (entnr: number) => {
+                    }), (entnr: number) => {
                         const ent = data.bp.entities.get(entnr)
                         if (ent.name === 'straight_rail') return ent
                     })
@@ -653,10 +659,10 @@ function generateGraphics(e: any) {
             }
             if (data.bp) {
                 const out = getBaseSprites()
-                const conn = data.bp.entityPositionGrid.getSurroundingEntities(new Area(data.position), (entnr: any, D: number) => {
-                    const entity = data.bp.entities.get(entnr)
-                    if (entity.name === 'stone_wall' && dir === D % 4) return true
-                })
+                const conn = data.bp.entityPositionGrid.getNeighbourData(data.position)
+                    .map(({ entity, relDir }) =>
+                        entity && entity.name === 'stone_wall' && dir === relDir % 4)
+
                 for (let i = 0; i < conn.length; i++) {
                     if (conn[i]) {
                         const wp = FD.entities['gate'].wall_patch[util.intToDir((i * 2 + 4) % 8)].layers[0]
@@ -673,25 +679,28 @@ function generateGraphics(e: any) {
         }
         case 'pipe': return (data: IEntityData) => {
             if (data.bp) {
-                const conn = data.bp.entityPositionGrid.getSurroundingEntities(new Area(data.position), (entnr: any, D: number) => {
-                    const entity = data.bp.entities.get(entnr)
-                    if (entity.name === 'pipe') return true
-                    if (entity.name === 'pipe_to_ground' && entity.direction === (D + 4) % 8) return true
-                    if ((entity.name === 'assembling_machine_2' || entity.name === 'assembling_machine_3') &&
-                        !entity.assemblerCraftsWithFluid) return false
-                    if (entity.name === 'chemical_plant' && entity.chemicalPlantDontConnectOutput &&
-                        entity.direction === D) return false
-                    const ed = entity.entityData
-                    if (ed.fluid_box || ed.output_fluid_box || ed.fluid_boxes) {
-                        const connections = getPipeConnectionPoints(ed, entity.direction, entity.assemblerPipeDirection)
-                        for (const connection of connections) {
-                            if (Math.floor(data.position.x) === Math.floor(entity.position.x + connection.x) &&
-                                Math.floor(data.position.y) === Math.floor(entity.position.y + connection.y)) {
-                                    return true
-                                }
+                const conn = data.bp.entityPositionGrid.getNeighbourData(data.position)
+                    .map(({ entity, relDir }) => {
+                        if (!entity) return false
+
+                        if (entity.name === 'pipe') return true
+                        if (entity.name === 'pipe_to_ground' && entity.direction === (relDir + 4) % 8) return true
+
+                        if ((entity.name === 'assembling_machine_2' || entity.name === 'assembling_machine_3') &&
+                            !entity.assemblerCraftsWithFluid) return false
+                        if (entity.name === 'chemical_plant' && entity.chemicalPlantDontConnectOutput &&
+                            entity.direction === relDir) return false
+
+                        if (entity.entityData.fluid_box || entity.entityData.output_fluid_box || entity.entityData.fluid_boxes) {
+                            const connections = getPipeConnectionPoints(entity.entityData, entity.direction, entity.assemblerPipeDirection)
+                            for (const connection of connections) {
+                                if (Math.floor(data.position.x) === Math.floor(entity.position.x + connection.x) &&
+                                    Math.floor(data.position.y) === Math.floor(entity.position.y + connection.y)) {
+                                        return true
+                                    }
+                            }
                         }
-                    }
-                })
+                    })
 
                 if (conn[0] && conn[1] && conn[2] && conn[3]) return [e.pictures.cross]
                 if (conn[0] && conn[1] && conn[3]) return [e.pictures.t_up]
@@ -799,15 +808,18 @@ function generateGraphics(e: any) {
         case 'transport_belt': return (data: IEntityData) => {
             const dir = data.dir
             function getBeltConnections() {
-                const directions = data.bp.entityPositionGrid.getSurroundingEntities(new Area(data.position), (entnr: number) => {
-                    const entity = data.bp.entities.get(entnr)
-                    if (entity.type === 'transport_belt' ||
-                        entity.type === 'splitter' ||
-                        (entity.type === 'underground_belt' &&
-                        entity.directionType === 'output')) {
-                            return entity.direction
-                        }
-                }, dir)
+                let directions = data.bp.entityPositionGrid.getNeighbourData(data.position)
+                    .map(({ entity }) => {
+                        if (!entity) return false
+
+                        if (
+                            entity.type === 'transport_belt' ||
+                            entity.type === 'splitter' ||
+                            (entity.type === 'underground_belt' && entity.directionType === 'output')
+                        ) return entity.direction
+                    })
+                // Rotate directions
+                directions = [...directions, ...directions].splice(dir / 2, 4)
 
                 const rightEntDir = directions[1]
                 const leftEntDir = directions[3]

src/actions.ts
@@ -103,7 +103,10 @@ const actions = {
     redo: new Action('modifier+y'),
     info: new Action('i'),
     pan: new Action('lclick'),
-    moveEntity: new Action('mclick'),
+    moveEntityUp: new Action('up'),
+    moveEntityLeft: new Action('left'),
+    moveEntityDown: new Action('down'),
+    moveEntityRight: new Action('right'),
     generateOilOutpost: new Action('g'),
 
     moveUp: new ToggleAction('w'),

src/app.ts
@@ -229,7 +229,7 @@ actions.closeWindow.bind(() => {
 })
 
 actions.inventory.bind(() => {
-    if (G.currentMouseState !== G.mouseStates.MOVING && !G.renderOnly) {
+    if (!G.renderOnly) {
         // If there is a dialog open, assume user wants to close it
         if (Dialog.anyOpen()) {
             Dialog.closeLast()
@@ -243,20 +243,16 @@ actions.inventory.bind(() => {
 actions.focus.bind(() => G.BPC.centerViewport())
 
 actions.rotate.bind(() => {
-    if (G.BPC.hoverContainer &&
-        (G.currentMouseState === G.mouseStates.NONE || G.currentMouseState === G.mouseStates.MOVING)
-    ) {
-        G.BPC.hoverContainer.rotate()
+    if (G.BPC.hoverContainer && G.currentMouseState === G.mouseStates.NONE) {
+        G.BPC.hoverContainer.entity.rotate(false, true)
     } else if (G.currentMouseState === G.mouseStates.PAINTING) {
         G.BPC.paintContainer.rotate()
     }
 })
 
 actions.reverseRotate.bind(() => {
-    if (G.BPC.hoverContainer &&
-        (G.currentMouseState === G.mouseStates.NONE || G.currentMouseState === G.mouseStates.MOVING)
-    ) {
-        G.BPC.hoverContainer.rotate(true)
+    if (G.BPC.hoverContainer && G.currentMouseState === G.mouseStates.NONE) {
+        G.BPC.hoverContainer.entity.rotate(true, true)
     } else if (G.currentMouseState === G.mouseStates.PAINTING) {
         G.BPC.paintContainer.rotate(true)
     }
@@ -332,18 +328,30 @@ actions.mine.bind(() => {
     }
 })
 
-actions.moveEntity.bind(() => {
-    if (!G.BPC.movingContainer && G.BPC.hoverContainer && G.currentMouseState === G.mouseStates.NONE) {
-        G.BPC.hoverContainer.pickUpEntityContainer()
-        return
+actions.moveEntityUp.bind(() => {
+    if (G.BPC.hoverContainer && G.currentMouseState === G.mouseStates.NONE) {
+        G.BPC.hoverContainer.entity.moveBy({ x: 0, y: -1 })
     }
-    if (G.BPC.movingContainer) {
-        G.BPC.movingContainer.placeDownEntityContainer()
+})
+actions.moveEntityLeft.bind(() => {
+    if (G.BPC.hoverContainer && G.currentMouseState === G.mouseStates.NONE) {
+        G.BPC.hoverContainer.entity.moveBy({ x: -1, y: 0 })
+    }
+})
+actions.moveEntityDown.bind(() => {
+    if (G.BPC.hoverContainer && G.currentMouseState === G.mouseStates.NONE) {
+        G.BPC.hoverContainer.entity.moveBy({ x: 0, y: 1 })
+    }
+})
+actions.moveEntityRight.bind(() => {
+    if (G.BPC.hoverContainer && G.currentMouseState === G.mouseStates.NONE) {
+        G.BPC.hoverContainer.entity.moveBy({ x: 1, y: 0 })
     }
 })
 
 actions.openEntityGUI.bind(() => {
     if (G.BPC.hoverContainer !== undefined) {
+        // console.log(G.BPC.hoverContainer.entity.getRawData())
         if (G.currentMouseState === G.mouseStates.NONE) {
             const editor = Editors.createEditor(G.BPC.hoverContainer.entity)
             if (editor === undefined) return

src/common/globals.ts
@@ -90,9 +90,8 @@ let book: Book | undefined
 
 const mouseStates = {
     NONE: 0,
-    MOVING: 1,
-    PAINTING: 2,
-    PANNING: 3
+    PAINTING: 1,
+    PANNING: 2
 }
 
 let currentMouseState = mouseStates.NONE

src/common/util.ts
@@ -112,7 +112,7 @@ function nearestPowerOf2(n: number) {
     return Math.pow(2, Math.ceil(Math.log2(n)))
 }
 
-function uniqueInArray(array: []) {
+function uniqueInArray(array: any[]) {
     return [...new Set(array)]
 }
 

src/containers/blueprint.ts
@@ -3,7 +3,6 @@ import { Viewport } from '../viewport'
 import { WiresContainer } from './wires'
 import { UnderlayContainer } from './underlay'
 import { EntitySprite } from '../entitySprite'
-import { AdjustmentFilter } from '@pixi/filter-adjustment'
 import { EntityContainer } from './entity'
 import { OverlayContainer } from './overlay'
 import { EntityPaintContainer } from './entityPaint'
@@ -21,13 +20,11 @@ export class BlueprintContainer extends PIXI.Container {
     underlayContainer: UnderlayContainer
     tiles: PIXI.Container
     entities: PIXI.Container
-    movingEntityFilter: AdjustmentFilter
     tileSprites: PIXI.Container
     entitySprites: PIXI.Container
     viewport: Viewport
     pgOverlay: PIXI.Graphics
     hoverContainer: undefined | EntityContainer
-    movingContainer: undefined | EntityContainer
     paintContainer: undefined | EntityPaintContainer | TilePaintContainer
 
     constructor() {
@@ -38,8 +35,6 @@ export class BlueprintContainer extends PIXI.Container {
             width: G.app.screen.width,
             height: G.app.screen.height
         }, 3)
-
-        this.movingEntityFilter = new AdjustmentFilter({ red: 0.4, blue: 0.4, green: 1 })
 
         this.generateGrid(G.colors.pattern)
 
@@ -110,7 +105,6 @@ export class BlueprintContainer extends PIXI.Container {
         }
 
         G.gridData.onUpdate(() => {
-            if (this.movingContainer) this.movingContainer.moveAtCursor()
             if (this.paintContainer) this.paintContainer.moveAtCursor()
 
             // Instead of decreasing the global interactionFrequency, call the over and out entity events here
@@ -153,19 +147,16 @@ export class BlueprintContainer extends PIXI.Container {
     initBP() {
         const firstRail = G.bp.getFirstRail()
         if (firstRail) {
-                G.railMoveOffset = {
+            G.railMoveOffset = {
                 x: Math.abs(firstRail.position.x) % 2 + 1,
                 y: Math.abs(firstRail.position.y) % 2 + 1
             }
         }
 
         // Render Bp
-        for (const entity_number of G.bp.entities.keys()) {
-            this.entities.addChild(new EntityContainer(G.bp.entities.get(entity_number), false))
-        }
-        G.bp.tiles.forEach((v, k) => {
-            this.tiles.addChild(new TileContainer(v, { x: Number(k.split(',')[0]), y: Number(k.split(',')[1]) }))
-        })
+        G.bp.entities.forEach(e => new EntityContainer(e, false))
+
+        G.bp.tiles.forEach((v, k) => new TileContainer(v, { x: Number(k.split(',')[0]), y: Number(k.split(',')[1]) }))
 
         this.sortEntities()
         this.wiresContainer.updatePassiveWires()
@@ -192,7 +183,6 @@ export class BlueprintContainer extends PIXI.Container {
         this.removeChildren()
 
         this.hoverContainer = undefined
-        this.movingContainer = undefined
         this.paintContainer = undefined
 
         this.tileSprites = new PIXI.Container()

src/containers/entity.ts
@@ -140,41 +140,61 @@ export class EntityContainer extends PIXI.Container {
 
         this.redraw(false, sort)
 
-        this.m_Entity.on('rotate', offset => {
-            if (G.currentMouseState === G.mouseStates.MOVING && offset) {
-                const pos = EntityContainer.getPositionFromData({
-                    x: this.x + offset.x * 32,
-                    y: this.y + offset.y * 32
-                }, this.m_Entity.size)
-                this.position.set(pos.x, pos.y)
-
-                G.BPC.overlayContainer.updateCursorBoxPosition(this.position)
-            }
-
-            this.redraw(G.currentMouseState === G.mouseStates.MOVING)
-
-            if (G.currentMouseState === G.mouseStates.NONE) this.redrawSurroundingEntities()
-
-            G.BPC.overlayContainer.updateCursorBoxSize(this.m_Entity.size.x, this.m_Entity.size.y)
+        this.m_Entity.on('rotate', () => {
+            this.redraw()
+            this.redrawSurroundingEntities()
             this.updateUndergroundLines()
-
-            if (G.BPC.movingContainer === this) this.checkBuildable()
-
             this.redrawEntityInfo()
             G.BPC.wiresContainer.update(this.m_Entity.entity_number)
         })
 
         this.m_Entity.on('recipe', () => {
             this.redrawEntityInfo()
-            if (this.m_Entity.name === 'chemical_plant' || this.m_Entity.assemblerCraftsWithFluid || this.m_Entity.assemblerCraftsWithFluid) {
+            if (this.m_Entity.name === 'chemical_plant' || this.m_Entity.assemblerCraftsWithFluid) {
                 this.redraw()
                 this.redrawSurroundingEntities()
             }
         })
 
-        this.m_Entity.on('modules', () => {
+        this.m_Entity.on('redraw', () => {
+            this.redraw()
+        })
+
+        this.m_Entity.on('destroy', () => {
+            this.destroy()
+        })
+
+        this.m_Entity.on('direction', () => {
+            this.redraw()
             this.redrawEntityInfo()
         })
+
+        this.m_Entity.on('directionType', () => {
+            this.redraw()
+            this.redrawSurroundingEntities()
+        })
+
+        this.m_Entity.on('modules', () => this.redrawEntityInfo())
+        this.m_Entity.on('filters', () => this.redrawEntityInfo())
+        this.m_Entity.on('splitterInputPriority', () => this.redrawEntityInfo())
+        this.m_Entity.on('splitterOutputPriority', () => this.redrawEntityInfo())
+
+        this.m_Entity.on('position', (newValue: IPoint, oldValue: IPoint) => {
+            console.log(newValue, oldValue)
+
+            this.position.set(
+                newValue.x * 32,
+                newValue.y * 32
+            )
+
+            this.redraw()
+            this.redrawSurroundingEntities()
+            this.updateUndergroundLines()
+            this.redrawEntityInfo()
+            G.BPC.wiresContainer.update(this.m_Entity.entity_number)
+        })
+
+        G.BPC.entities.addChild(this)
     }
 
     public get entity(): Entity {
@@ -182,12 +202,18 @@ export class EntityContainer extends PIXI.Container {
     }
 
     destroy() {
-        // TODO: Check if the following line is actually still necessary
-        // if (G.editEntityContainer.visible) G.editEntityContainer.close()
+        G.BPC.wiresContainer.remove(this.m_Entity.entity_number)
+
+        this.redrawSurroundingEntities()
+
+        G.BPC.hoverContainer = undefined
+
+        G.BPC.wiresContainer.updatePassiveWires()
+
+        G.BPC.updateOverlay()
 
         for (const s of this.entitySprites) s.destroy()
 
-        super.destroy()
         EntityContainer.mappings.delete(this.m_Entity.entity_number)
 
         UnderlayContainer.modifyVisualizationArea(this.areaVisualization, s => s.destroy())
@@ -195,25 +221,8 @@ export class EntityContainer extends PIXI.Container {
         G.BPC.overlayContainer.hideUndergroundLines()
 
         if (this.entityInfo !== undefined) this.entityInfo.destroy()
-    }
 
-    checkBuildable() {
-        const position = EntityContainer.getGridPosition(this.position)
-        if (!EntityContainer.isContainerOutOfBpArea(position, this.m_Entity.size) &&
-            G.bp.entityPositionGrid.checkNoOverlap(this.m_Entity.name, this.m_Entity.direction, position)
-        ) {
-            G.BPC.movingEntityFilter.red = 0.4
-            G.BPC.movingEntityFilter.green = 1
-        } else {
-            G.BPC.movingEntityFilter.red = 1
-            G.BPC.movingEntityFilter.green = 0.4
-        }
-    }
-
-    rotate(ccw = false) {
-        const rotateOpposingEntity = G.currentMouseState === G.mouseStates.NONE && this.m_Entity.type === 'underground_belt'
-        const offset = G.gridData.calculateRotationOffset(this.position)
-        this.m_Entity.rotate(G.currentMouseState === G.mouseStates.NONE, offset, ccw, rotateOpposingEntity)
+        super.destroy()
     }
 
     updateUndergroundLines() {
@@ -243,56 +252,6 @@ export class EntityContainer extends PIXI.Container {
         }
     }
 
-    updateVisualStuff() {
-        for (const s of this.entitySprites) s.setPosition(this.position)
-
-        UnderlayContainer.modifyVisualizationArea(this.areaVisualization, s => s.position.copy(this.position))
-
-        if (this.entityInfo !== undefined) this.entityInfo.position = this.position
-
-        G.BPC.overlayContainer.updateCursorBoxPosition(this.position)
-        G.BPC.overlayContainer.updateUndergroundLinesPosition(this.position)
-        this.updateUndergroundLines()
-
-        G.BPC.wiresContainer.update(this.m_Entity.entity_number)
-
-        this.checkBuildable()
-    }
-
-    removeContainer() {
-        G.BPC.wiresContainer.remove(this.m_Entity.entity_number)
-        G.bp.entityPositionGrid.removeTileData(this.m_Entity.entity_number, false)
-        this.redrawSurroundingEntities()
-        G.bp.removeEntity(this.m_Entity.entity_number,
-            entity_number => EntityContainer.mappings.get(entity_number).redraw()
-        )
-        G.BPC.hoverContainer = undefined
-
-        G.BPC.wiresContainer.updatePassiveWires()
-
-        G.BPC.updateOverlay()
-        this.destroy()
-    }
-
-    moveAtCursor() {
-        const position = G.gridData.position
-        if (G.BPC.movingContainer === this && G.currentMouseState === G.mouseStates.MOVING) {
-            switch (this.m_Entity.name) {
-                case 'straight_rail':
-                case 'curved_rail':
-                case 'train_stop':
-                    this.x = position.x - (position.x + G.railMoveOffset.x * 32) % 64 + 32
-                    this.y = position.y - (position.y + G.railMoveOffset.y * 32) % 64 + 32
-                    break
-                default:
-                    const pos = EntityContainer.getPositionFromData(position, this.m_Entity.size)
-                    this.position.set(pos.x, pos.y)
-            }
-
-            this.updateVisualStuff()
-        }
-    }
-
     pointerOverEventHandler() {
         if (G.currentMouseState === G.mouseStates.NONE) {
             G.BPC.hoverContainer = this
@@ -316,45 +275,6 @@ export class EntityContainer extends PIXI.Container {
         }
     }
 
-    pickUpEntityContainer() {
-        G.bp.entityPositionGrid.removeTileData(this.m_Entity.entity_number, false)
-        this.redraw(true)
-        this.redrawSurroundingEntities()
-        G.BPC.movingContainer = this
-        G.currentMouseState = G.mouseStates.MOVING
-
-        // Move container to cursor
-        const pos = EntityContainer.getPositionFromData(G.gridData.position, this.m_Entity.size)
-        if (this.position.x !== pos.x || this.position.y !== pos.y) {
-            this.position.set(pos.x, pos.y)
-            this.updateVisualStuff()
-        }
-
-        for (const s of this.entitySprites) s.moving = true
-        G.BPC.sortEntities()
-        G.BPC.underlayContainer.activateRelatedAreas(this.m_Entity.name)
-
-        G.BPC.updateOverlay()
-    }
-
-    placeDownEntityContainer() {
-        const position = EntityContainer.getGridPosition(this.position)
-        if (EntityContainer.isContainerOutOfBpArea(position, this.m_Entity.size)) return
-        if (G.currentMouseState === G.mouseStates.MOVING && this.m_Entity.move(position)) {
-            G.BPC.movingContainer = undefined
-            G.currentMouseState = G.mouseStates.NONE
-
-            for (const s of this.entitySprites) s.moving = false
-
-            this.redraw(false)
-            this.redrawSurroundingEntities()
-
-            G.BPC.underlayContainer.deactivateActiveAreas()
-
-            G.BPC.updateOverlay()
-        }
-    }
-
     redrawSurroundingEntities() {
         if (!updateGroups[this.m_Entity.name]) return
         if (this.m_Entity.name === 'straight_rail') {
@@ -363,16 +283,9 @@ export class EntityContainer extends PIXI.Container {
                 if (ent.name === 'gate') EntityContainer.mappings.get(ent.entity_number).redraw()
             })
         } else {
-            const redrawnEntities: number[] = []
-            updateGroups[this.m_Entity.name].forEach((updateGroup: string[]) => {
-                G.bp.entityPositionGrid.getSurroundingEntities(this.m_Entity.getArea(), (entnr: number) => {
-                    const ent = G.bp.entities.get(entnr)
-                    if (updateGroup.includes(ent.name) && !redrawnEntities.includes(entnr)) {
-                        EntityContainer.mappings.get(ent.entity_number).redraw()
-                        redrawnEntities.push(entnr)
-                    }
-                })
-            })
+            G.bp.entityPositionGrid.getSurroundingEntities(this.m_Entity.getArea())
+                .filter(entity => updateGroups[this.m_Entity.name].includes(entity.name))
+                .forEach(entity => EntityContainer.mappings.get(entity.entity_number).redraw())
         }
     }
 
@@ -380,7 +293,6 @@ export class EntityContainer extends PIXI.Container {
         for (const s of this.entitySprites) s.destroy()
         this.entitySprites = []
         for (const s of EntityContainer.getParts(this.m_Entity, G.hr, ignore_connections)) {
-            if (G.BPC.movingContainer === this) s.moving = true
             s.setPosition(this.position)
             this.entitySprites.push(s)
             G.BPC.entitySprites.addChild(s)

commit 96239a361a6bd761045eb2f06b9614bcf8bc575b

fixed bug with heat pipe connection introduced in d77240e

src/factorio-data/spriteDataBuilder.ts
@@ -245,15 +245,13 @@ function getHeatConnections(position: IPoint, bp: Blueprint) {
 
             if (entity.name === 'heat_pipe') return true
             if (entity.name === 'heat_exchanger' || entity.name === 'nuclear_reactor') {
-                getHeatConectionPoints(entity.entityData)
+                return getHeatConectionPoints(entity.entityData)
                     .map(conn => util.rotatePointBasedOnDir(conn.position, entity.direction))
-                    .forEach(offset => {
-                        if (x === Math.floor(entity.position.x + offset.x) &&
-                            y === Math.floor(entity.position.y + offset.y)) return true
-                    })
+                    .filter(offset =>
+                        x === Math.floor(entity.position.x + offset.x) &&
+                        y === Math.floor(entity.position.y + offset.y))
+                    .length > 0
             }
-
-            return false
         })
 }
 