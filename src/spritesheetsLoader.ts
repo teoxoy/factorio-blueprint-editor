443880314e6e75f15191b8ea0f0a4762290d0fe8
Teoxoy <28601907+Teoxoy@users.noreply.github.com>
2019-02-20 23:26:28 +0100

    use createImageBitmap to decode spritesheets async

--- src/spritesheetsLoader.ts
+++ src/spritesheetsLoader.ts
@@ -47,26 +47,37 @@ function changeQuality(hr: boolean) {
 }
 
 function loadSpritesheet(src: string, json: any) {
-    return new Promise((resolve, reject) => {
-        const image = new Image()
-        image.src = src
-        image.onload = () => {
+    return fetch(src)
+        .then(response => response.blob())
+        .then(blob => {
+            if (!!window.createImageBitmap) return createImageBitmap(blob)
+
+            // Polyfill
+            return new Promise(resolve => {
+                const img = new Image()
+                img.onload = () => resolve(img)
+                img.src = URL.createObjectURL(blob)
+            })
+        })
+        .then((imageData: ImageBitmap | HTMLImageElement) => {
             const getPow2Canvas = () => {
                 const c = document.createElement('canvas')
-                c.width = util.nearestPowerOf2(image.width)
-                c.height = util.nearestPowerOf2(image.height)
-                c.getContext('2d').drawImage(image, 0, 0)
+                c.width = util.nearestPowerOf2(imageData.width)
+                c.height = util.nearestPowerOf2(imageData.height)
+                c.getContext('2d').drawImage(imageData, 0, 0)
                 return c
             }
             // if WebGL1, make the spritesheet a power of 2 so that it generates mipmaps
             // WebGL2 generates mipmaps even with non pow 2 textures
-            const baseTexture = new PIXI.BaseTexture(G.app.renderer.context.webGLVersion === 1 ? getPow2Canvas() : image)
+            const resource = G.app.renderer.context.webGLVersion === 1
+                ? new PIXI.resources.CanvasResource(getPow2Canvas())
+                : new PIXI.resources.BaseImageResource(imageData)
+
+            const baseTexture = new PIXI.BaseTexture(resource)
             // bind the baseTexture, this will also upload it to the GPU
             G.app.renderer.texture.bind(baseTexture)
-            new PIXI.Spritesheet(baseTexture, json).parse(resolve)
-        }
-        image.onerror = reject
-    })
+            return new Promise(resolve => new PIXI.Spritesheet(baseTexture, json).parse(resolve))
+        })
 }
 
 export default {
