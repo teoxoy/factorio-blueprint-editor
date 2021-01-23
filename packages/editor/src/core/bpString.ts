70f0e7581fd56f800626bc8bcdaaf4556c9792c6
teoxoy <28601907+teoxoy@users.noreply.github.com>
2021-01-23 14:56:40 +0100

    encode URI component

--- packages/editor/src/core/bpString.ts
+++ packages/editor/src/core/bpString.ts
@@ -152,7 +152,7 @@ function getBlueprintOrBookFromSource(source: string): Promise<Blueprint | Book>
             const pathParts = url.pathname.slice(1).split('/')
 
             const fetchData = (url: string): Promise<Response> =>
-                fetch(`__CORS_PROXY_URL__${url}`).then(response => {
+                fetch(`__CORS_PROXY_URL__${encodeURIComponent(url)}`).then(response => {
                     if (response.ok) return response
                     throw new Error('Network response was not ok.')
                 })
