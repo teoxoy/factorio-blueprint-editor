b706c5a25cf0c6d3cece1b669353ff4133ccf5fe
SighnWaive <sighnwaive@gmail.com>
2024-04-08 05:19:11 -0400

    Update to Factorio 1.1.104

--- packages/exporter/src/main.rs
+++ packages/exporter/src/main.rs
@@ -6,7 +6,7 @@ mod setup;
 #[macro_use]
 extern crate lazy_static;
 
-static FACTORIO_VERSION: &str = "1.1.100";
+static FACTORIO_VERSION: &str = "1.1.104";
 
 lazy_static! {
     static ref DATA_DIR: PathBuf = PathBuf::from("./data");
